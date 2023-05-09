2c41d648272c447a86c58d365cb201e1e3c161a0
mitsukanpon <mtkad0331@gmail.com>
2023-05-09 23:28:27 +0900

    create project and model

---
+++ database/migrations/2023_05_09_215901_create_tasks.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->text('title', 50);
+            $table->text('contents', 200);
+            $table->integer('complete_flg')->default(0);
+            $table->datetime("due_date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
